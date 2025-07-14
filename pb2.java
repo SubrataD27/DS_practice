891c6a057dc757c15500fd261469ef59ebad90b0
SubrataD27 <dhibarsubrata18@gmail.com>
2025-07-15 00:13:20 +0530

    2nd

---
+++ pb2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Solution {
+    public void printNumber(Scanner sc) {
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();         // Read input from user
+        System.out.println("Output: " + number);  // Print the number
+    }
+}
+
+public class pb2 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {   // Try-with-resources
+            Solution sol = new Solution();            // Create Solution object
+            sol.printNumber(sc);                      // Call the method
+        }
+    }
+}
