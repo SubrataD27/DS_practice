38843531dc795a95847bb972e028ea44a9b39f3e
SubrataD27 <dhibarsubrata18@gmail.com>
2025-07-19 20:36:25 +0530

    d-2-1

---
+++ striver_playlist/Day 1/2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+    cout << "Value of y:" << y << "and x:" << x;
+    return 0;
+}
---
+++ striver_playlist/Day 1/learnc++.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+    std::cout << "Hey Steiver" << "\n";
+    std::cout << "Hey Striver!" << std::endl << "Hey Raj!" << std::endl;    
+    std::cout << "Hey Striver!";
+    return 0;
+}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout << "Hey Steiver" << "\n";
+//     cout << "Hey Striver!" << endl << "Hey Raj!" << endl;    
+//     cout << "Hey Striver!";
+//     return 0;
+// }
