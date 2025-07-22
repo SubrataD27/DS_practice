7490716520530d274e5b8d93e1fbddf8b661f3a4
SubrataD27 <dhibarsubrata18@gmail.com>
2025-07-22 23:37:21 +0530

    day4 l

---
+++ striver_playlist/Day 4/p2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class p2{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0 ; i < n ; i++){
+            for( char ch = 'A' ; ch <= 'A' + i ; ch++)
+            {
+            System.out.print(ch + " ");
+        }
+        System.out.println();
+    }
+}
+}
